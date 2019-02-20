9e028343eed837f3d7566b978e974ae9ebf77762
Matthew Walls <matt.walls31@gmail.com>
2019-02-20 00:03:28 -0800

    Initial XML Push
    Contains lots of skeleton design. Will fill out with details going forward.

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Cthulhu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Estate")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2017 (Jecrell)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
+
+[assembly: AssemblyVersion("1.1.0.7")]
+[assembly: AssemblyFileVersion("1.1.0.7")]
---
+++ Source/REUtility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RERimhazard
+{
+    public static class REUtility
+    {
+    }
+}
