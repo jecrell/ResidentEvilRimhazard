743f21235387ed8709bf64fb17959fae72bdb83c
Matthew Walls <matt.walls31@gmail.com>
2019-02-24 00:05:47 -0800

    Created formula for healing when mixing herbs

---
+++ Source/IngestionOutcomeDoer_Herb.cs
@@ -0,0 +1,57 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RERimhazard
+{
+    public class IngestionOutcomeDoer_Herb : IngestionOutcomeDoer
+    {
+        
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            var nameOfMixedHerbThingdef = ingested.def.defName;
+
+
+            //Determine number of "greens" mixed in
+            var numOfGreenHerbsMixedIn = FindNumberOfSubstrings(nameOfMixedHerbThingdef, "Green");
+
+            //Determine number of "reds" mixed in
+            var redHerbFactor = 1 + FindNumberOfSubstrings(nameOfMixedHerbThingdef, "Red");
+
+            //Determine number of "blues" mixed in
+            var blueHerbFactor = 1 + FindNumberOfSubstrings(nameOfMixedHerbThingdef, "Blue");
+
+            switch (numOfGreenHerbsMixedIn)
+            {
+                case 0:
+                    return;
+                case 1:
+                    REUtility.HealAddedPercentage(pawn, 0.15f * (redHerbFactor + blueHerbFactor));
+                    break;
+                case 2:
+                    REUtility.HealAddedPercentage(pawn, 0.3f * (redHerbFactor + blueHerbFactor));
+                    break;
+                case 3:
+                    REUtility.HealAddedPercentage(pawn, 0.9f * (redHerbFactor + blueHerbFactor));
+                    break;
+                default:
+                    REUtility.HealAddedPercentage(pawn, 0.15f * (redHerbFactor + blueHerbFactor));
+                    break;
+            }
+        }
+
+        private static int FindNumberOfSubstrings(string mainString, string subString)
+        {
+            int numOfSubstrings = 0;
+            while (mainString.Contains(subString))
+            {
+                mainString.ReplaceFirst(subString, "");
+                numOfSubstrings++;
+            }
+            return numOfSubstrings;
+        }
+    }
+}
--- Source/REUtility.cs
+++ Source/REUtility.cs
@@ -2,10 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using Verse;
 
 namespace RERimhazard
 {
     public static class REUtility
     {
+        /// <summary>
+        /// Heals a character a certain added percentage
+        /// </summary>
+        /// <param name="injuredPawn">The character to be healed</param>
+        /// <param name="percentage">The additional percentage to heal, for example: 30%. If the current injuries are about 40%, the total health percentage will be boosted until 70%.</param>
+        public static void HealAddedPercentage(Pawn injuredPawn, float percentage)
+        {
+            //Added healing is capped at 100%, and if the percentage is lower, exit
+            percentage = Mathf.Clamp(percentage, 0, 1.0f);
+            if (percentage <= 0) return;
+
+            //Our character must exist and have calculatable health
+            if (injuredPawn == null) return;
+            if (!injuredPawn.Spawned) return;
+            if (injuredPawn.health == null) return;
+            if (injuredPawn.health.summaryHealth == null) return;
+
+            
+            //Find a new maximum healing percentage
+            float maximumHealingPct = Mathf.Clamp(injuredPawn.health.summaryHealth.SummaryHealthPercent + percentage, 0f, 1.0f);
+            
+            //Heal each injury until the healing percentage is at or above the maximum healing percentage
+            foreach (var injury in injuredPawn.health.hediffSet.GetInjuriesTendable())
+            {
+                if (injuredPawn.health.summaryHealth.SummaryHealthPercent >= maximumHealingPct)
+                    break;
+                injury.Heal(99999f);
+            }
+
+        }
+        
     }
 }
